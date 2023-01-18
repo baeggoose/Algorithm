c985df4d1b97a8caeeeed24fc9aad9f4990c9406
baeggoose <bsj154@gmail.com>
2023-01-18 18:32:01 +0900

    JS100 10.별 찍기 답안 추가

--- JS10.js
+++ JS10.js
@@ -10,6 +10,21 @@
 //  *******
 // *********
 
+// 답안1
+let k = prompt("숫자를 입력하세요");
+
+for (let i = 1; i <= k; i++) {
+  let tree = "";
+  for (let h = 1; h <= k - i; h++) {
+    tree += " ";
+  }
+  for (let j = 1; j <= i * 2 - 1; j++) {
+    tree += "*";
+  }
+  console.log(tree);
+}
+
+// 답안2
 let k = prompt("숫자를 입력하세요");
 let tree = "";
 
@@ -24,6 +39,23 @@ for (let i = 0; i < k; i++) {
     tree += "*";
   }
   tree += "\n";
+}
+
+console.log(tree);
+
+// 답안3
+const n = prompt("숫자를 입력하세요.");
+let tree = "";
+
+for (let i = 1; i <= n; i++) {
+  let star = "";
+  for (let j = 1; j <= n - i; j++) {
+    star += " ";
+  }
+  for (let k = 1; k <= 2 * i - 1; k++) {
+    star += "*";
+  }
+  tree += star + "\n";
 }
 
 console.log(tree);
