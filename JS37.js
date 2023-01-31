9fbc887bfd08f3810c62a6a1df39e094b0c8fa48
baeggoose <bsj154@gmail.com>
2023-01-31 15:10:56 +0900

    JS100 37.반장 선거 다시풀어보기

--- JS37.js
+++ JS37.js
@@ -5,3 +5,14 @@
 // 원범 원범 혜원 혜원 혜원 혜원 유진 유진
 // 출력
 // 혜원(이)가 총 4표로 반장이 되었습니다.
+const array = prompt("학생 입력").split(" ");
+let result = {};
+let winner = "";
+for (let index in array) {
+  let val = array[index];
+  result[val] = result[val] === undefined ? 1 : (result[val] = result[val] + 1);
+}
+winner = Object.keys(result).reduce(function (a, b) {
+  return result[a] > result[b] ? a : b;
+});
+console.log(`${winner}(이)가 총 ${result[winner]}표로 반장이 되었습니다.`);
