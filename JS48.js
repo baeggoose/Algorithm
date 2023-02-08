eec92e2d86f94948eb32ee306cad7f71c819ad6d
baeggoose <bsj154@gmail.com>
2023-02-08 18:42:41 +0900

    JS100 48.대소문자 바꿔서 출력하기

--- JS48.js
+++ JS48.js
@@ -2,10 +2,13 @@
 // 문자열이 주어지면 대문자와 소문자를 바꿔서 출력하는 프로그램을 작성하세요.
 // 입력 : AAABBBcccddd
 // 출력 : aaabbbCCCDDD
-const n = prompt("문자 입력");
-const upper = n.toUpperCase();
-if (n === upper) {
-  n.toUpperCase();
-} else {
-  n.toLowerCase();
+let str = prompt("문자 입력");
+let arr = [];
+for (let i = 0; i < str.length; i++) {
+  if (str[i] === str[i].toLowerCase()) {
+    arr.push(str[i].toUpperCase());
+  } else {
+    arr.push(str[i].toLowerCase());
+  }
 }
+console.log(arr.join(""));
