3fd2ca99d098717e907c2b353cd8d033938a653f
baeggoose <bsj154@gmail.com>
2023-02-21 21:57:28 +0900

    JS100 56.객체의 함수 응용

--- JS56.js
+++ JS56.js
@@ -1,7 +1,7 @@
 // # 문제56 : 객체의 함수 응용
 // 다음의 객체가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력하세요.
 // 데이터
-nationWidth = {
+const nationWidth = {
   korea: 220877,
   Rusia: 17098242,
   China: 9596961,
@@ -12,9 +12,3 @@ nationWidth = {
 
 // 출력
 // England 22023
-function near() {
-  for (i = 0; i < nationWidth.length; i++) {
-    nationWidth[i] < [i + 1];
-  }
-}
-console.log(near(korea));
