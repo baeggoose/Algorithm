e119fc9f3a71570f25ef3e4abd380a51c6381b2c
baeggoose <bsj154@gmail.com>
2023-02-23 16:50:16 +0900

    JS100 55.하노이의 탑 다시

--- JS55.js
+++ JS55.js
@@ -23,11 +23,11 @@ function hanoi(num, start, end, temp) {
   }
 
   //원반이 n-1개를 경유기둥으로 옮기고
-  hanoi(num - 1, start, temp, end);
+  hanoi(/*내용을 채워주세요.*/);
   //가장 큰 원반은 목표기둥으로
-  route.push([start, end]);
+  route.push(/*내용을 채워주세요.*/);
   //경유기둥과 시작기둥을 바꿉니다.
-  hanoi(num - 1, temp, end, start);
+  hanoi(/*내용을 채워주세요.*/);
 }
 
 hanoi(3, "A", "B", "C");
