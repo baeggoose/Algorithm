b4ec53156d2afbeeb7fa3d3d3f025adae828e2cb
baeggoose <bsj154@gmail.com>
2023-03-08 16:50:16 +0900

    JS100 56.객체의 함수 응용

--- JS56.js
+++ JS56.js
@@ -1,6 +1,7 @@
 // # 문제56 : 객체의 함수 응용
 // 다음의 객체가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력하세요.
-// 데이터
+// 출력
+// England 22023
 const nationWidth = {
   korea: 220877,
   Rusia: 17098242,
@@ -10,13 +11,20 @@ const nationWidth = {
   England: 242900,
 };
 
-// 출력
-// England 22023
-
-const k = nationWidth["koreaß"];
+const k = nationWidth["korea"];
 delete nationWidth["korea"];
 
 const entry = Object.entries(nationWidth);
 const values = Object.values(nationWidth);
 
-let gap = Math.max.abs(nationWidth);
+let gap = Math.max.apply(null, values);
+let item = [];
+
+for (let i in entry) {
+  if (gap > Math.abs(entry[i][0] - k)) {
+    gap = Math.abs(entry[i][0] - k);
+    item = entry[i];
+  }
+}
+
+console.log(item[0], item[1] - k);
