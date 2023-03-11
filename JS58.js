0ad67d593b9938c1fbf20e6dd6155f36de19ba9d
baeggoose <bsj154@gmail.com>
2023-03-11 09:26:42 +0900

    JS100 58.콤마찍기 재귀함수이용

--- JS58.js
+++ JS58.js
@@ -4,6 +4,15 @@
 // **숫자를 입력받고 천 단위로 콤마(,)를 찍어주세요.**
 // 예를 들어, **123456789**를 입력받았으면 **123,456,789**를 출력해야 합니다.
 
-// 내장함수 이용 방법
+// 내장함수 이용
 const cash = prompt("정산금을 입력하세요");
 console.log(cash.toLocaleString);
+
+// 재귀함수 이용
+function comma(a) {
+  if (a.length <= 3) {
+    return a;
+  } else {
+    return comma();
+  }
+}
