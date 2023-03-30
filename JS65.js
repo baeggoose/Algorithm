5c39d3cca2db0762577b9cbb0b70b6e3f931cca5
baeggoose <bsj154@gmail.com>
2023-03-31 08:59:48 +0900

    JS100 65.변형된 리스트 map 이용

--- JS65.js
+++ JS65.js
@@ -2,8 +2,11 @@
 let a = [1, 2, 3, 4];
 let b = ["a", "b", "c", "d"];
 // 이런 리스트가 있을 때 [[1, a], [b, 2], [3, c], [d, 4]] 이런 식으로 a, b 리스트가 번갈아가면서 출력되게 해주세요.
-for (let i = 0; i < a.length; i++) {
-  for (let j = 0; j < b.length; j++) {
-    console.log(a[i] + b[j]);
+let result = a.map(function (e, i) {
+  if (i % 2 == 0) {
+    return [e, b[i]];
+  } else {
+    return [b[i], e];
   }
-}
+});
+console.log(result);
