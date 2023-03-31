b6aa6ce30717936fc2bbab748939f36c5a0d81ca
baeggoose <bsj154@gmail.com>
2023-03-31 09:29:55 +0900

    JS100 65.변형된 리스트 forEach, map 풀이추가

--- JS65.js
+++ JS65.js
@@ -2,6 +2,8 @@
 let a = [1, 2, 3, 4];
 let b = ["a", "b", "c", "d"];
 // 이런 리스트가 있을 때 [[1, a], [b, 2], [3, c], [d, 4]] 이런 식으로 a, b 리스트가 번갈아가면서 출력되게 해주세요.
+
+// map 으로 푸는 방법
 let result = a.map(function (e, i) {
   if (i % 2 == 0) {
     return [e, b[i]];
@@ -10,3 +12,15 @@ let result = a.map(function (e, i) {
   }
 });
 console.log(result);
+
+// forEach 로 푸는 방법
+
+let c = [];
+a.forEach(function (e, i) {
+  if (i % 2 == 0) {
+    c.push([e, b[i]]);
+  } else {
+    c.push([b[i], e]);
+  }
+});
+console.log(c);
